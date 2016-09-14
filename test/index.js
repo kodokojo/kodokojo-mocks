9409928834d60f61b6327599009cd5c7b67b5477
Huljo <huljopub@gmail.com>
2016-09-14 14:28:02 +0200

    randomize test server port to prevent double binding closing server after each test to prevent multiple port binding add a test to cover route convertion

--- test/index.js
+++ test/index.js
@@ -9,14 +9,15 @@ describe('Server', function() {
   describe('Run', function() {
     it('Should start and return server state ready', function() {
       var server = new mockServer({
-        port: 8090,
+        port: Math.floor(Math.random() * 9000) + 8000  ,
         logs: false,
         routes: []
       });
       return server.start().then(function(state){
-          expect(state).to.be.an('object');
-          expect(state).to.include.keys('ready');
-          expect(state.ready).to.be.true;
+        expect(state).to.be.an('object');
+        expect(state).to.include.keys('ready');
+        expect(state.ready).to.be.true;
+        server.close();
       })
     });
   });
@@ -24,10 +25,22 @@ describe('Server', function() {
     it('Should read and parse config file', function() {
       var server = new mockServer(__dirname+"/test_config.json");
       return server.start().then(function(state){
-          expect(server.config).to.be.an('object');
-          expect(server.config.routes).to.be.an('array');
-          expect(server.config.routes).to.deep.include({"path":"/user","method":"POST","mockType":"raw","serve":"0821b5c16a367e5df4044b183af3f0d18235d832"});
-      })
+        expect(server.config).to.be.an('object');
+        expect(server.config.routes).to.be.an('array');
+        expect(server.config.routes).to.deep.include({"path":"/user","method":"POST","mockType":"raw","serve":"0821b5c16a367e5df4044b183af3f0d18235d832"});
+        server.close();
+      });
+    });
+    it('Should convert routes from config file to restify routes', function() {
+      var server = new mockServer(__dirname+"/test_config.json");
+      return server.start().then(function(state){
+        var recordedPostRoute = server.restifyServer.router.mounts.postapiv1user.spec;
+        expect(recordedPostRoute).to.be.an('object');
+        expect(recordedPostRoute).to.include.keys('path');
+        expect(recordedPostRoute.path).to.be.equal('api/v1/user');
+        expect(recordedPostRoute.method).to.be.equal('POST');
+        server.close();
+      });
     });
   });
 });
