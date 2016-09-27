4a6b97250651707f4cc6c0426968abab47699957
Huljo <huljopub@gmail.com>
2016-09-27 16:38:51 +0200

    remove custom test file

--- test.js
+++
@@ -1,7 +0,0 @@
-var mockServer = require('./lib/index.js');
-
-var server = new mockServer(__dirname+"/test/test_mocks/test_config.json");
-return server.start().then(function(state){
- //console.log(state);
- //console.log(server.config);
-});
