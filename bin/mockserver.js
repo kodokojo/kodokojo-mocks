4937ae477a50815e31fda9576beebb5153b1d42c
Huljo <huljopub@gmail.com>
2016-12-13 13:07:25 +0100

    add cli

---
+++ bin/mockserver.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict';
+var meow = require('meow');
+var mockServer = require('../lib/index.js');
+
+var templates = {
+    usage: 'Usage'+
+    '  $ mockserver <config_file_path>'+
+    '\n\r'+
+    '\n\r'+
+    'Examples'+
+    '\n\r'+
+    '    $ mockserver config.json'
+};
+
+var cli = meow(templates.usage, {
+    alias: {}
+});
+//
+if(typeof cli.input[0] === 'undefined') {
+    console.log('Missing configuration path');
+} else {
+    var server = new mockServer(cli.input[0]);
+    server.start().then(function(state){
+        // console.log(123);
+    })
+}
